324b8139e8956acdbd6bb3df0343f3931332886c
Nikolai Schimke <NikolaiSch@tuta.io>
2024-01-30 18:26:42 +0000

    Add route for getting active streams

--- src/bin/web_server.rs
+++ src/bin/web_server.rs
@@ -4,7 +4,9 @@ use scraper::{db, models::Stream};
 #[tokio::main]
 async fn main() {
     // build our application with a single route
-    let app = Router::new().route("/", get(get_all_streams));
+    let app = Router::new()
+        .route("/", get(get_all_streams))
+        .route("/active", get(get_active_streams));
 
     // run our app with hyper, listening globally on port 3000
     let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
@@ -12,8 +14,17 @@ async fn main() {
 }
 
 async fn get_all_streams() -> Json<Vec<Stream>> {
+    println!("get_all_streams");
     let mut conn = db::establish_connection();
     let streams = db::get_streams(&mut conn);
+
+    Json(streams)
+}
+
+async fn get_active_streams() -> Json<Vec<Stream>> {
+    println!("get_active_streams");
+    let mut conn = db::establish_connection();
+    let streams = db::get_linked_streams(&mut conn);
 
     Json(streams)
 }
--- src/models.rs
+++ src/models.rs
@@ -1,8 +1,9 @@
 use diesel::prelude::*;
+use serde::ser::SerializeStruct;
 use serde::Deserialize;
 use serde::Serialize;
 
-#[derive(Debug, Queryable, Serialize, Deserialize)]
+#[derive(Debug, Queryable, Deserialize)]
 pub struct Stream {
     pub id: Option<i32>,
     pub away: String,
@@ -24,4 +25,23 @@ pub struct StreamNew<'a> {
     pub country: &'a str,
     pub url: &'a str,
     pub stream_link: &'a str,
+}
+
+impl Serialize for Stream {
+    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
+    where
+        S: serde::ser::Serializer,
+    {
+        let split_streams: Vec<&str> = self.stream_link.split(",").collect();
+        let mut stream = serializer.serialize_struct("Stream", 8)?;
+        stream.serialize_field("id", &self.id)?;
+        stream.serialize_field("home", &self.home)?;
+        stream.serialize_field("away", &self.away)?;
+        stream.serialize_field("start_time", &self.start_time)?;
+        stream.serialize_field("league", &self.league)?;
+        stream.serialize_field("country", &self.country)?;
+        stream.serialize_field("url", &self.url)?;
+        stream.serialize_field("stream_link", &split_streams)?;
+        stream.end()
+    }
 }
