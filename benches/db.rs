80d5c7847bba4575215eae922141d1d9a40b3fff
Nikolai Schimke <NikolaiSch@tuta.io>
2024-01-31 23:23:56 +0000

    fixing db result errors

--- benches/db.rs
+++ benches/db.rs
@@ -2,18 +2,16 @@ use criterion::{criterion_group, criterion_main, Criterion};
 use scraper::db;
 
 fn create_connection() {
-    db::establish_connection();
+    db::establish_connection().unwrap();
 }
 
 fn criterion_benchmark(c: &mut Criterion) {
     // 43.6 nano seconds
-    c.bench_function("Create new db connection", |b| {
-        b.iter(create_connection)
-    });
+    c.bench_function("Create new db connection", |b| b.iter(create_connection));
 
     // 228.3 nano seconds
     c.bench_function("Create a new record", |b| {
-        let mut conn = db::establish_connection();
+        let mut conn = db::establish_connection().unwrap();
         let new_stream = scraper::models::StreamNew {
             away: "Away",
             home: "Home",
@@ -28,7 +26,7 @@ fn criterion_benchmark(c: &mut Criterion) {
 
     // 356 nano seconds
     c.bench_function("Get all streams", |b| {
-        let mut conn = db::establish_connection();
+        let mut conn = db::establish_connection().unwrap();
         b.iter(|| db::get_streams(&mut conn))
     });
 }
