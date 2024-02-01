336045b1f9e52b7859cedc2d9d55d00305193945
Nikolai Schimke <94403162+NikolaiSch@users.noreply.github.com>
2024-02-01 15:36:55 +0000

    adding new integration tests for db

--- tests/common/mod.rs
+++ tests/common/mod.rs
@@ -1,7 +1,11 @@
+use anyhow::Error;
+use diesel::SqliteConnection;
 use scraper::db::helpers::establish_test_connection;
 
 
-pub mod create_db() -> Result<(), Error> {
-   let test_db = establish_test_connection(); 
+pub fn create_db() -> Result<SqliteConnection, Error> {
+    let test_db = establish_test_connection()?;
 
+
+    Ok(test_db)
 }
--- tests/test_db.rs
+++ tests/test_db.rs
@@ -1,1 +1,32 @@
+use chrono::NaiveDateTime;
+use diesel::SqliteConnection;
+use scraper::db::{helpers::create_stream, models::StreamNew};
+
 mod common;
+
+
+fn test_create_stream() -> Result<SqliteConnection, anyhow::Error> {
+    common::create_db()
+}
+
+#[test]
+fn test_insert_stream() -> Result<(), anyhow::Error> {
+    let mut test_db = test_create_stream()?;
+
+    let new_stream = StreamNew {
+        home: "Arsenal",
+        away: "Chelsea",
+        sport: "Football",
+        start_time: NaiveDateTime::from_timestamp_opt(1612128000, 0).unwrap(),
+        stream_link: "https://www.youtube.com/watch?v=dQw4w9WgXcQ,https://www.youtube.com/watch?v=dQw4w9WgXcQ",
+        country: "England",
+        league: "Premier League",
+        url: "https://www.youtube.com/watch?v=dQw4w9WgXcQ",
+    };
+
+    let result = create_stream(&mut test_db, &new_stream)?;
+
+    assert_eq!(result, 1);
+
+    Ok(())
+}
