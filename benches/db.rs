f25442c45d36c855d765ca1c7b4be21278c4c17f
Nikolai Schimke <NikolaiSch@tuta.io>
2024-02-01 00:00:23 +0000

    Update database helper functions

--- benches/db.rs
+++ benches/db.rs
@@ -2,7 +2,7 @@ use criterion::{criterion_group, criterion_main, Criterion};
 use scraper::db;
 
 fn create_connection() {
-    db::establish_connection().unwrap();
+    db::helpers::establish_connection().unwrap();
 }
 
 fn criterion_benchmark(c: &mut Criterion) {
@@ -11,8 +11,8 @@ fn criterion_benchmark(c: &mut Criterion) {
 
     // 228.3 nano seconds
     c.bench_function("Create a new record", |b| {
-        let mut conn = db::establish_connection().unwrap();
-        let new_stream = scraper::models::StreamNew {
+        let mut conn = db::helpers::establish_connection().unwrap();
+        let new_stream = db::models::StreamNew {
             away: "Away",
             home: "Home",
             league: "League",
@@ -21,13 +21,13 @@ fn criterion_benchmark(c: &mut Criterion) {
             url: "Url",
             stream_link: "https://www.test.com",
         };
-        b.iter(|| db::create_stream(&mut conn, &new_stream))
+        b.iter(|| db::helpers::create_stream(&mut conn, &new_stream))
     });
 
     // 356 nano seconds
     c.bench_function("Get all streams", |b| {
-        let mut conn = db::establish_connection().unwrap();
-        b.iter(|| db::get_streams(&mut conn))
+        let mut conn = db::helpers::establish_connection().unwrap();
+        b.iter(|| db::helpers::get_streams(&mut conn))
     });
 }
 
